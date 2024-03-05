e53b999784d3b3b9a6522f89dfab0ed0db2581ef
kobiowuquadri <kobiowuq@gmail.com>
2024-03-05 08:14:05 -0600

    add merket place section

---
+++ src/Components/MarketTraders/MarketTraders.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react'
+import marketTraderImg from '../../assets/market_traders.png'
+import smallBusinessImg from '../../assets/small_business.png'
+import comperativesImg from '../../assets/comperatives.png'
+import privateOfficersImg from '../../assets/private_officers.png'
+import publicOfficesImg from '../../assets/public_offices.png'
+import './markettraders.scss'
+
+function MarketTraders () {
+  const [selectedItem, setSelectedItem] = useState(0)
+
+  const itemsData = [
+    {
+      item: 'Market Traders',
+      title: 'Do you belong to a Market Association?',
+      description:
+        'we believe everyone should have access to money at the time they need it without exorbitant interest rates that makes it hard to return.',
+      image: marketTraderImg
+    },
+    {
+      item: 'Small Businesses',
+      title: 'Or you own a small business?',
+      description:
+        'we believe everyone should have access to money at the time they need it without exorbitant interest rates that makes it hard to return.',
+      image: smallBusinessImg
+    },
+    {
+      item: 'Comperatives',
+      title: 'Cooperatives are not left out',
+      description:
+        'we believe everyone should have access to money at the time they need it without exorbitant interest rates that makes it hard to return.',
+      image: comperativesImg
+    },
+    {
+      item: 'Private Officers',
+      title: 'Halal also works with private officers',
+      description:
+        'we believe everyone should have access to money at the time they need it without exorbitant interest rates that makes it hard to return.',
+      image: privateOfficersImg
+    },
+    {
+      item: 'Public Offices',
+      title: 'Halal also works with public officers',
+      description:
+        'we believe everyone should have access to money at the time they need it without exorbitant interest rates that makes it hard to return.',
+      image: publicOfficesImg
+    }
+  ]
+
+  const handleItemClick = index => {
+    setSelectedItem(index)
+  }
+
+  return (
+    <div className='traders_section'>
+      <ul
+        id='tarders_list'
+        className='d-flex justify-content-between list-unstyled' style={{cursor:'pointer'}}
+      >
+        {itemsData.map((item, index) => (
+          <li key={index} onClick={() => handleItemClick(index)}>
+            {item.item}
+          </li>
+        ))}
+      </ul>
+      <div className='d-flex main_content'>
+        <div className='market_content'>
+          <h1>{itemsData[selectedItem].title}</h1>
+          <p>{itemsData[selectedItem].description}</p>
+          <button className='btn btn-outline-secondary text-primary'>
+            Learn More
+          </button>
+        </div>
+        <div className='market_image'>
+          <img
+            src={itemsData[selectedItem].image}
+            className='img-fluid'
+            alt=''
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MarketTraders
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../../Components/Header/Header'
 import Hero from '../../Components/Hero/Hero'
 import Partners from '../../Components/Partners/Partners'
+import MarketTraders from '../../Components/MarketTraders/MarketTraders'
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Header/>
       <Hero/>
       <Partners/>
+      <MarketTraders/>
     </>
   )
 }
